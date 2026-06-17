8078c39013a6b3d9ad9d31bb288d980ac65d242b
ffranca1984-ieq <mrfsites@gmail.com>
2026-06-17 09:49:32 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'floresceria-v88';
+const CACHE = 'floresceria-v90';
 const ASSETS = ['/', '/index.html', '/painel.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
